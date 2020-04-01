a91f579907252f0b2056ec7de5e1e6df6b27b2a3
Altin Terstena <alt-2020@outlook.com>
2020-03-31 22:37:42 -0400

    Initial commit

---
+++ app/src/main/java/com/al/peteranswers/PeterAnswersActivity.kt
@@ -0,0 +1,12 @@
+package com.al.peteranswers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PeterAnswersActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_peter_answers)
+    }
+}
